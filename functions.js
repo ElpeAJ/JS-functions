e5b7149c5f15df36c8d00eb5074a20ec285c9166
Elpedia Arthur <elpediaarthurjunior@gmail.com>
2023-12-05 00:33:58 -0800

    Create a function that checks if the input/parameter is an even number

---
+++ functions.js
@@ -0,0 +1,24 @@
+// Create a function that checks if the input/parameter is an even number.
+
+
+function isItEven(i) {
+  while (i >= 0) {
+    if (i % 2 == 0) {
+      console.log(`${i} is even`);
+    } else {
+      console.log(`${i} is odd`);
+    }
+    break;
+  }
+}
+
+isItEven(4);
+isItEven(3);
+isItEven(50);
+
+// Create a function that prints to the console n1 to n2. where n1 is the first input/parameter and n2 is the second input/parameter
+
+
+
+
+// Create a function that sum the values in-between two numbers. eg calcFunc(1,3) returns 6. ie. 1+2+3.
