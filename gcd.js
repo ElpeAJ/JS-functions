5257c52800304e43f0640f6e28fbc814b001dedb
Elpedia Arthur <elpediaarthurjunior@gmail.com>
2023-12-05 05:28:07 -0800

    Finding the Greatest Common Factor

---
+++ gcd.js
@@ -0,0 +1,12 @@
+// Number Pyramid:Create a script that prints a number pyramid pattern,
+// where each row contains numbers in ascending order.
+// Greatest Common Divisor (GCD) Calculator:Write a function 
+// that calculates the greatest common divisor (GCD) of two numbers.
+
+10 = 1, 2, 5, 10 
+ 
+20 = 1, 2, 4, 5, 10, 20
+
+divisor = 1, 2, 5, 10
+
+GCD = 10
