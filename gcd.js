127f2a58f13c6a8d366815554dc2ff4bda549b70
Elpedia Junior Arthur <elpediaarthurjunior@gmail.com>
2024-01-23 14:53:54 -0800

    Creating a program that checks the gcd of two numbers

--- gcd.js
+++ gcd.js
@@ -1,12 +1,10 @@
-// Number Pyramid:Create a script that prints a number pyramid pattern,
-// where each row contains numbers in ascending order.
 // Greatest Common Divisor (GCD) Calculator:Write a function 
 // that calculates the greatest common divisor (GCD) of two numbers.
 
-10 = 1, 2, 5, 10 
+let ten = [1, 2, 5, 10 ];
  
-20 = 1, 2, 4, 5, 10, 20
+let twenty = [1, 2, 4, 5, 10, 20];
 
-divisor = 1, 2, 5, 10
+let divisor = [1, 2, 5, 10];
 
-GCD = 10
+GCD = 10;
